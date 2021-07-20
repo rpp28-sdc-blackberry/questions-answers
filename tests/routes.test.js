5002691d6bf846ae1a43ac69eb08b8af1cdce40c
jabrake <jorge.brake@gmail.com>
2021-07-20 16:26:00 -0700

    Added basic unit tests for routes

---
+++ tests/routes.test.js
@@ -0,0 +1,41 @@
+const request = require('supertest');
+
+describe('GET requests to the reviews endpoint', () => {
+  it('respond with 200', (done) => {
+    request('http://localhost:3000')
+      .get('/reviews')
+      .expect(200, done);
+  });
+});
+
+describe('GET requests to the meta endpoint', () => {
+  it('respond with 200', (done) => {
+    request('http://localhost:3000')
+      .get('/reviews/meta')
+      .expect(200, done);
+  });
+});
+
+describe('POST requests to the reviews endpoint', () => {
+  it('respond with 201', (done) => {
+    request('http://localhost:3000')
+      .post('/reviews')
+      .expect(201, done);
+  });
+});
+
+describe('PUT requests to the helpful endpoint', () => {
+  it('respond with 204', (done) => {
+    request('http://localhost:3000')
+      .put('/reviews/16/helpful')
+      .expect(204, done);
+  });
+});
+
+describe('PUT requests to the report endpoint', () => {
+  it('respond with 204', (done) => {
+    request('http://localhost:3000')
+      .put('/reviews/16/report')
+      .expect(204, done);
+  });
+});

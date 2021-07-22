4d6787ab06fb86cc3fca41f26bd03f10ef507da6
jabrake <jorge.brake@gmail.com>
2021-07-21 19:23:09 -0700

    Implemented route and query to return data when accessing reviews endpoint, sending data back in expected shape, accounting for wrong parameters

--- db/index.js
+++ db/index.js
@@ -13,18 +13,27 @@ client.connect()
   .then(() => console.log('Connected to Postgres server!'))
   .catch((error) => console.log('Could not connect to Postgres server:', error));
 
-const testFunction = function () {
-  return new Promise((resolve, reject) => {
-    client.query('SELECT * FROM reviews')
-      .then((data) => {
-        console.log('got data in DB file:', data);
-        resolve(data);
-      })
-      .catch((error) => {
-        console.log('error retrieving data:', error);
-        reject(error);
-      });
-  });
+const getReviews = (productId, sort, offset, count) => {
+  let sortValue;
+  if (sort === 'newest') {
+    sortValue = 'date';
+  } else if (sort === 'helpful') {
+    sortValue = 'helpfulness';
+  } else if (sort === 'relevant') {
+    sortValue = 'rating';
+  } else { console.log('invalid sort value'); }
+
+  // Need to implement an algorithm to sort by relevance somehow
+  const query = {
+    text: `SELECT * FROM reviews WHERE product_id = $1 ORDER BY ${sortValue} DESC OFFSET $2 LIMIT $3`,
+    values: [productId, offset, count],
+  };
+
+  return client.query(query)
+    .then((res) => res.rows)
+    .catch((error) => error.stack);
 };
 
-module.exports = { testFunction };
+// const getMeta =
+
+module.exports = { getReviews };
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('../db/index');
 
 const app = express();
 
@@ -6,12 +7,32 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ strict: false }));
 
 app.get('/reviews', (req, res) => {
-  const productId = req.query.product_id;
-  const page = req.query.page || 1;
-  const count = req.query.count || 5;
+  const productId = Number(req.query.product_id);
+  const page = Number(req.query.page) || 1;
+  const count = Number(req.query.count) || 5;
   const sort = req.query.sort || 'newest';
+  const offset = (page - 1) * count;
 
-  res.status(200).send(`Success hitting reviews endpoint with product ID: ${productId}, page: ${page}, count: ${count}, sort: ${sort}`);
+  if (page < 0 || count < 0) {
+    res.status(400).send('Invalid syntax, make sure page and count parameters are not less than 0');
+    return;
+  }
+
+  db.getReviews(productId, sort, offset, count)
+    .then((reviews) => {
+      const resObject = {
+        product: productId,
+        page,
+        count,
+        results: reviews,
+      };
+
+      res.status(200).send(resObject);
+    })
+    .catch((error) => {
+      console.log('error getting reviews in server:', error);
+      res.status(500).send('error getting reviews in server');
+    });
 });
 
 app.get('/reviews/meta', (req, res) => {

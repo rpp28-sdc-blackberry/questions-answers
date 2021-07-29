36c94ec968f92361b12dd5567eab578d3ce53ad8
jabrake <jorge.brake@gmail.com>
2021-07-29 16:57:23 -0700

    Updated environment variable names for Postgres connection

--- db/index.js
+++ db/index.js
@@ -6,18 +6,18 @@ let database;
 
 if (process.env.NODE_ENV === 'development') {
   // user = process.env.PG_USER;
-  database = process.env.PG_DATABASE;
+  database = process.env.POSTGRES_DB;
 } else if (process.env.NODE_ENV === 'test') {
   // user =
   database = 'reviews_test';
 }
 
 const pool = new Pool({
-  user: process.env.PG_USER,
+  user: process.env.POSTGRES_USER,
   database,
-  password: process.env.PG_PASSWORD,
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
 });
 
 module.exports = pool;

fbd8e3af9386f6fc62736b574de3fb4f2632d80a
jabrake <jorge.brake@gmail.com>
2021-07-29 19:29:20 -0700

    Updated tests for PUT routes

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -13,12 +13,12 @@ beforeAll(async () => {
   await db.query('ALTER SEQUENCE characteristics_reviews_id_seq RESTART');
 
   // eslint-disable-next-line quotes
-  await db.query(`INSERT INTO reviews (product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness) VALUES (1, 5, current_timestamp, 'test summary', 'test body', true, false, 'test reviewer name', 'test reviewer email', 'test response', 16)`);
+  await db.query(`INSERT INTO reviews (product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness) VALUES (1, 5, current_timestamp, 'test summary', 'test body', true, false, 'test reviewer name', 'test reviewer email', 'test response', 1)`);
   // eslint-disable-next-line quotes
   await db.query(`INSERT INTO photos (review_id, url) VALUES (1, 'www.testurl.com')`);
   // eslint-disable-next-line quotes
   await db.query(`INSERT INTO characteristics (product_id, name) VALUES (1, 'test characteristic')`);
-  await db.query('INSERT INTO characteristics_reviews (characteristic_id, review_id, value) VALUES (1, 1, 16)');
+  await db.query('INSERT INTO characteristics_reviews (characteristic_id, review_id, value) VALUES (1, 1, 1)');
 });
 
 afterAll(async () => {
@@ -77,14 +77,30 @@ describe('POST requests to the reviews endpoint', () => {
 
 describe('PUT requests to the /helpful endpoint', () => {
   it('should respond with 204', async () => {
-    const response = await request.put('/reviews/1/helpful');
-    expect(response.statusCode).toBe(204);
+    const reviewId = 1;
+    let getResponse = await db.query(`SELECT helpfulness FROM reviews WHERE id = ${reviewId}`);
+    let helpfulness = getResponse.rows[0].helpfulness;
+    expect(helpfulness).toEqual(1);
+
+    const putResponse = await request.put(`/reviews/${reviewId}/helpful`);
+    getResponse = await db.query(`SELECT helpfulness FROM reviews WHERE id = ${reviewId}`);
+    helpfulness = getResponse.rows[0].helpfulness;
+    expect(putResponse.statusCode).toBe(204);
+    expect(helpfulness).toEqual(2);
   });
 });
 
 describe('PUT requests to the /report endpoint', () => {
   it('should respond with 204', async () => {
-    const response = await request.put('/reviews/1/report');
-    expect(response.statusCode).toBe(204);
+    const reviewId = 1;
+    let getResponse = await db.query(`SELECT reported FROM reviews WHERE id = ${reviewId}`);
+    let reported = getResponse.rows[0].reported;
+    expect(reported).toBe(false);
+
+    const putResponse = await request.put(`/reviews/${reviewId}/report`);
+    getResponse = await db.query(`SELECT reported FROM reviews WHERE id = ${reviewId}`);
+    reported = getResponse.rows[0].reported;
+    expect(putResponse.statusCode).toBe(204);
+    expect(reported).toBe(true);
   });
 });
